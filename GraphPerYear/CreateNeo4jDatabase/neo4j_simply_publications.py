1aaabc9cb9aaab6c1ce73904db7539d03699f62d
Sergi <saguilocastillo@gmail.com>
2021-03-09 18:12:52 +0100

    Return name of the tool and keywords

--- GraphPerYear/CreateNeo4jDatabase/neo4j_simply_publications.py
+++ GraphPerYear/CreateNeo4jDatabase/neo4j_simply_publications.py
@@ -1,6 +1,7 @@
 from neo4j import GraphDatabase
 import time
 import json
+from owlready2 import *
 
 # Start time - Just to count how much the script lasts
 start_time = time.time()
@@ -11,21 +12,67 @@ uri = "bolt://localhost:7687"
 # To be able to use it, you need to open your neo4j server before
 driver = GraphDatabase.driver(uri, auth=("neo4j", "1234"))
 
+def retrieve_keywords(i):
+    set_keywords = set()
+    if "semantics" not in i:
+        return False
+    for inputs in i["semantics"]["inputs"]:
+        if "datatype" in inputs:
+            set_keywords.add(inputs["datatype"])
+        for formats in inputs["formats"]:
+            set_keywords.add(formats)
+    for operations in i["semantics"]["operations"]:
+        set_keywords.add(operations)
+    for outputs in i["semantics"]["outputs"]:
+        if "datatype" in outputs:
+            set_keywords.add(outputs["datatype"])
+        for formats in outputs["formats"]:
+            set_keywords.add(formats) 
+    for topics in i["semantics"]["topics"]:
+        set_keywords.add(topics)
+    set_names= set()
+    for keywords in set_keywords:
+        name=keywords.split("/")[3]
+        if "topic" in name:
+            Ids = f"onto.{name}.hasHumanReadableId"
+            try:
+                Ids=eval(Ids)
+                for Id in Ids:
+                    Id=Id.replace("_", " ")
+                    set_names.add(Id)
+            except:
+                continue
+            continue
+        label = f"onto.{name}.label"
+        labels = eval(label)
+        for label in labels:
+            label = label.replace("_", " ")
+            set_names.add(label)
+    print(set_names)
+    return list(set_names)
+    
+    
 # This function search in the JSON file if one of the IDs matches a publication of a tool.
 def search_json(data,doi,pmid,pmcid):
     for i in data:
         if "publications" not in i:
             continue
+        if "(EBI)" in i["name"]:
+            continue
         for ids in i["publications"]:
             if doi != "None" and "doi" in ids:
                 if doi in ids["doi"]:
-                    return i["name"]
+                    keywords=retrieve_keywords(i)
+                    return i["name"], keywords
             if pmid != "None" and "pmid" in ids:
                 if pmid in ids["pmid"]:
-                    return i["name"]
+                    keywords=retrieve_keywords(i)
+                    return i["name"], keywords
             if pmcid != "None" and "pmcid" in ids:
                 if pmcid in ids["pmcid"]:
-                    return i["name"]
+                    keywords=retrieve_keywords(i)
+                    return i["name"], keywords
+    return False, False
 
 def graph():
     with driver.session() as session:
@@ -35,7 +82,7 @@ def graph():
             WHERE size((n)--())=0
             DELETE (n)
             """)
-    try:
+
         # Take publications that do not have any edge with a Tool node
         publications = session.run("""
             match (n)
@@ -51,19 +98,23 @@ def graph():
             for i in publications:
                 counter += 1
                 print(counter)
-                name_tool= search_json(data, i["doi"], i["pmid"], i["pmcid"]) # Input the IDs of the publication from different platforms
+                name_tool, keywords= search_json(data, i["doi"], i["pmid"], i["pmcid"]) # Input the IDs of the publication from different platforms
                 if not name_tool:
                     continue
                 # If the tool is found, we can input it in the database
+                l_keywords = []
+                if keywords:
+                    l_keywords = list(keywords)
+                print(i["id"], name_tool, l_keywords)
                 query ="""
                     match (n:Publication {id: "%s"})
-                    create (n)-[:HAS_TOOL]->(:InferedTool {name:"%s"})
-                    """ % (i["id"], name_tool)
+                    create (n)-[:HAS_TOOL]->(:InferedTool {name:"%s", keywords: %s})
+                    """ % (i["id"], name_tool, l_keywords)
                 session.run(query)
-    except:
-        print("Doesn't work")
 
 if __name__ == '__main__':
+    # Import ontology
+    onto = get_ontology("http://edamontology.org/EDAM.owl").load()
     graph()
     print("--- %s seconds ---" % (time.time() - start_time))
 
