78eeafe5850b14c404dd023848922b8d72a0ad94
Sergi <saguilocastillo@gmail.com>
2021-03-31 09:44:16 +0200

    Add clustering nodes and its relations

--- MetaGraph/CreateNeo4jDatabase/data_science.py
+++ MetaGraph/CreateNeo4jDatabase/data_science.py
@@ -13,65 +13,94 @@ driver = GraphDatabase.driver(uri, auth=("neo4j", "1234"))
 
 def graph():
     with driver.session() as session:
+        # Remove nodes with no interactions
         session.run("""
             MATCH (n)
             WHERE size((n)--())=0
             DELETE (n)
             """)
+        # Remove previous nodes and edges
+        session.run("""MATCH ()-[r:METAOCCUR_COMM]->() DELETE r""")
+        session.run("""MATCH ()-[r:HAS_COMMUNITY]->() DELETE r""")
+        session.run("""MATCH (r:Community) DELETE r""")
+        
+        #Remove previous graphs
+        #session.run("""
+            #CALL gds.graph.drop('got-weighted-interactions')
+        #""")
+        
+        print("Creating view")
         ### PageRank
-        # Creating the "view"
-        session.run("""
-            CALL gds.graph.create(
-                'got-interactions', 
-                'InferedTool', 
-                {
-                    METAOCCUR: 
-                    {
-                        orientation: 'UNDIRECTED'
-                    }
-                }
-            )
-            """)
-        # Write PageRank values to each node
-        session.run("""
-            CALL gds.pageRank.write(
-                'got-interactions', 
-                {
-                    writeProperty: 'pageRank'
-                }
-            )
-            """)
-        ### Louvain weighted
         # Create view with the property
         session.run("""
             CALL gds.graph.create(
             'got-weighted-interactions',
-            'InferedTool',
+            ['InferedTool', 'Publication'],
             {
                 METAOCCUR: {
-                orientation: 'UNDIRECTED',
-                aggregation: 'NONE',
-                properties: {
-                    times: {
-                    property: 'times',
+                    orientation: 'UNDIRECTED',
                     aggregation: 'NONE',
-                    defaultValue: 0.0
+                    properties: {
+                        times: {
+                        property: 'times',
+                        aggregation: 'NONE',
+                        defaultValue: 0.0
+                        }
                     }
-                }
                 }
             }
             )
             """)
+        print("PageRank")
+        # Write PageRank values to each node
+        session.run("""
+            CALL gds.pageRank.write(
+                'got-weighted-interactions', 
+                {
+                    relationshipWeightProperty: 'times',
+                    writeProperty: 'pageRank'
+                }
+            )
+            """)
+        print("Louvain")
         # Write the community id to each node
         session.run("""
             CALL gds.louvain.write(
-            'got-weighted-interactions',
-            {
-                relationshipWeightProperty: 'times',
-                writeProperty: 'community'
-            }
+                'got-weighted-interactions',
+                {
+                    relationshipWeightProperty: 'times',
+                    writeProperty: 'community'
+                }
             )
             """)
+        
+        # Create clusters as nodes
+        session.run("""
+            MATCH (n) 
+            WITH distinct n.community as com 
+            CREATE (:Community {com_id: com})
+            """)
+        # Edges between nodes and its communities
+        session.run("""
+            MATCH (n),(i:Community) 
+            WHERE n.community = i.com_id
+            CREATE (n)-[:HAS_COMMUNITY]->(i)
+            """)
+        session.run("""
+            MATCH (c2:Community)<-[h2:HAS_COMMUNITY]-(p)-[m:METAOCCUR_ALL]-(n)-[h:HAS_COMMUNITY]->(c1:Community)
+            WHERE c1<> c2
+            WITH c2,c1, collect(m) as co
+                UNWIND co as c 
+            WITH c2, sum(c.times) as sumo , c1
+            CREATE (c1)-[:METAOCCUR_COMM {times: sumo}]->(c2)
+            """)
+        # Delete duplicated and reversed relationships
+        session.run("""
+            Match (c1:Community)-[r:METAOCCUR_COMM]->(c2:Community)
+            where c1.com_id < c2.com_id
+            delete r
+            """)
+        
 
 
 if __name__ == '__main__':
