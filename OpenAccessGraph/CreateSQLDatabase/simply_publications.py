8ca486fd38a883fb8c62a702c7e671d2cde9be44
Sergi <saguilocastillo@gmail.com>
2021-04-14 10:12:43 +0200

    Create index for searching faster the publications

--- OpenAccessGraph/CreateSQLDatabase/simply_publications.py
+++ OpenAccessGraph/CreateSQLDatabase/simply_publications.py
@@ -8,7 +8,7 @@ from os import walk
 start_time = time.time()
 
 # Name of the database
-DB_FILE = "database/OAMolecular.db"
+DB_FILE = "database/OAComparative.db"
 
 # Connect to the SQLite database
 # If name not found, it will create a new database
@@ -32,13 +32,17 @@ c.execute('''CREATE TABLE IF NOT EXISTS "Publications" (
 	            "doi" TEXT,
 	            PRIMARY KEY("id")
             )''')
+c.execute('''
+    CREATE UNIQUE INDEX idx_Publications
+    ON Publications (doi, pmcid, pmid);
+    ''')
 
 ### Import data
 counter = 0 # Dummy counter
 
-#mypath = "PubEnricher/opeb-enrichers-master/pubEnricher/output/pruebaSAguilo"
+mypath = "PubEnricher/opeb-enrichers-master/pubEnricher/output/pruebaSAguilo"
 #mypath = "PubEnricher/pruebaSAguiloAPE_2"
-mypath = "PubEnricher/pruebaSAguiloeuro_MoL"
+#mypath = "PubEnricher/pruebaSAguiloeuro_MoL"
 
 
 _, dirnames, _ = next(walk(mypath))
