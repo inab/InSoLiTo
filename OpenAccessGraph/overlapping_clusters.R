ea152f7b4f3beee5368a5c963bf814251ae1caf8
Sergi <saguilocastillo@gmail.com>
2021-05-08 16:15:18 +0200

    File with overlap and animations

--- OpenAccessGraph/overlapping_clusters.R
+++ OpenAccessGraph/overlapping_clusters.R
@@ -6,13 +6,14 @@ library(visNetwork)
 library(networkD3)
 library(data.table)
 library(ndtv)
+library(dplyr)
+
 
 
 setwd("~/Escritorio/TFM/SoLiTo/Meta_OA/")
 
-
 overlapping_sections = function(section){
-  edges_file = read.table("edge_relations.txt", sep= "\t",quote = "", header = T)
+  edges_file = read.table("edge_relations_proteomics.txt", sep= "\t",quote = "", header = T)
   edges_file_all = edges_file[edges_file$Section== "All",]
   edges_file_all$is_section = FALSE 
   edges_file_section = edges_file[edges_file$Section== section,]
@@ -87,6 +88,8 @@ overlapping_sections = function(section){
               length(E(g)$is_section[E(g)$is_section == TRUE])/length(E(g)$is_section)))
   visIgraph(g_simp, randomSeed = 123)
   
+  #return(method)
+  
 }
 
 overlapping_sections("Introduction")
@@ -94,48 +97,8 @@ overlapping_sections("Methods")
 overlapping_sections("Results")
 overlapping_sections("Discussion")
 
-######################## Animation ######################
+################# Network animation #########################
 
-
-label_nodes = data.frame("id" = as.character(methods_nodes[,1]),
-                         "comm" = as.character(methods_nodes[,2]),
-                         "label" =  as.character(methods_nodes[,1]))
-label_nodes$id = as.character(label_nodes$id)
-label_nodes$comm = as.character(label_nodes$comm)
-label_nodes$label = as.character(label_nodes$label)
-
-c(1:20)[as.factor(label_nodes$comm)]
-
-edges_file_methods_net = edges_file_methods[,c(1,2,3)]
-colnames(edges_file_methods_net) = c("from", "to", "weight")
-
-g3 = network(edges_file_methods_net, matrix.type = "edgelist",
-             vertex.attr = label_nodes,
-             loops=F, multiple=F,
-             ignore.eval = F, directed = F )
-g3
-plot(g3)
-
-g3 %n% "net.name" = "Methods co-occurrences" #  network attribute
-g3 %v% "comm"    # Node attribute
-g3 %e% "weight"     # Node attribute
-
-g3 %v% "col" = rainbow(length(unique(g3 %v% "comm")))[as.factor(g3 %v% "comm")]
-
-plot(g3,vertex.col = "col")
-
-
-render.d3movie(g3, usearrows = F, displaylabels = F,
-               displaylabels=TRUE,
-               vertex.border="#ffffff", vertex.col =  g3 %v% "col",
-               #vertex.cex = (net3 %v% "audience.size")/8, 
-               edge.lwd = (g3 %e% "weight")/10, edge.col = '#55555599',
-               vertex.tooltip = paste("<b>Name:</b>", (g3 %v% 'id') , "<br>",
-                                      "<b>Comm:</b>", (g3 %v% 'comm')),
-               edge.tooltip = paste("<b>Edge weight:</b>", (g3 %e% "weight" )),
-               launchBrowser=F,filename="Methods-Network.html" )
-
-# Animation
 
 animation_data = read.table("year_edges.txt", header = T, sep = "\t")
 
@@ -143,11 +106,21 @@ animation_vertices = data.frame("name" = c(as.character(animation_data$name_i), 
                                 "pageRank" = c(animation_data$pageRank_i, animation_data$pageRank_p),
                                 "community" = c(animation_data$community_i, animation_data$community_p),
                                 "start_year" = c(animation_data$year, animation_data$year),
-                                "end_year" = c(animation_data$year_end, animation_data$year_end))
+                                "end_year" = c(animation_data$year_end, animation_data$year_end),
+                                "node_type" = c(as.character(animation_data$type_i), as.character(animation_data$type_p)))
 animation_vertices_unique=setDT(animation_vertices)[ , .SD[which.min(start_year)], by = name]   # Unique with min year
 
 animation_vertices_unique$id = c(1:nrow(animation_vertices_unique))
 animation_vertices_unique$name = as.character(animation_vertices_unique$name)
+animation_vertices_unique$node_type = as.character(animation_vertices_unique$node_type)
+
+
+animation_vertices_unique$subname = animation_vertices_unique$name
+for(i in 1:nrow(animation_vertices_unique)){
+  if((nchar(animation_vertices_unique$name[i]) > 15)  & animation_vertices_unique$node_type[i]=="Publication"){
+    animation_vertices_unique$subname[i] = paste(substr(animation_vertices_unique$name[i],1,15), "...", sep="")
+  }
+}
 
 animation_edges = data.frame("from" = animation_data$name_i,
                              "to" = animation_data$name_p,
@@ -159,10 +132,62 @@ animation_edges$from = as.character(animation_edges$from)
 animation_edges$to = as.character(animation_edges$to)
 animation_vertices_unique$name = as.character(animation_vertices_unique$name)
 
+for(i in 1:nrow(animation_edges)){
+  from_node = as.character(animation_edges$from[i])
+  to_node = as.character(animation_edges$to[i])
+  if(from_node < to_node){
+    animation_edges$from[i] = to_node
+    animation_edges$to[i] = from_node 
+  }
+}
+
+# Table sorted
+animation_edges=animation_edges[order(animation_edges$from, animation_edges$to, animation_edges$start),]
+
+animation_edges=animation_edges%>%group_by(from, to, )%>%mutate(cumusum=cumsum(weight))
+
+animation_edges = as.data.frame(animation_edges)
+
+previous_row = animation_edges[1,]
+for(i in 2:nrow(animation_edges)){
+  if(animation_edges[i, 1] == previous_row[1] &
+     animation_edges[i, 2] == previous_row[2]){
+
+    if(as.integer(animation_edges[i,3]) > as.integer(previous_row[3] + 1)){
+      print(paste(animation_edges[i,3], (previous_row[3] + 1)))
+      diff_years = animation_edges[i,3] - (previous_row[3] + 1)
+      print(paste(diff_years, animation_edges[i,1], animation_edges[i,2],
+            previous_row[1], previous_row[2]))
+      for(j in 1:(diff_years[1,1])){
+        new_row = data.frame(previous_row[1],previous_row[2], previous_row[3] + j,
+                             previous_row[4],0, previous_row[6])
+        colnames(new_row) = c("from", "to", "start", "end", "weight", "cumusum")
+        print(new_row)
+        animation_edges= rbind(animation_edges, new_row)
+      }
+    }
+  }
+  if((animation_edges[i, 1] != previous_row[1] |
+     animation_edges[i, 2] != previous_row[2]) &
+     (previous_row[3]<2020)){
+    diff_years = (2020 - previous_row[3])
+    print(paste(2, previous_row[1], previous_row[2], diff_years))
+    for(j in 1:(diff_years[1,1])){
+      new_row = data.frame(previous_row[1],previous_row[2], previous_row[3] + j,
+                           previous_row[4],0, previous_row[6])
+      colnames(new_row) = c("from", "to", "start", "end", "weight", "cumusum")
+      print(new_row)
+      animation_edges= rbind(animation_edges, new_row)
+    }
+  }
+  previous_row=animation_edges[i,]
+}
+
+animation_edges=animation_edges[order(animation_edges$from, animation_edges$to, animation_edges$start),]
+
+
 animation_edges$end = animation_edges$start + 1
 animation_vertices_unique$end_year = 2021
-
-
 
 for(i in 1:nrow(animation_edges)){
   for(j in 1:nrow(animation_vertices_unique)){
@@ -178,48 +203,10 @@ for(i in 1:nrow(animation_edges)){
 animation_edges$from = as.numeric(animation_edges$from)
 animation_edges$to = as.numeric(animation_edges$to)
 animation_edges$weight = as.numeric(animation_edges$weight)
+animation_edges$cumusum= as.numeric(animation_edges$cumusum)
 
-
-
-g_netdyinamic = networkDynamic(vertex.spells = animation_vertices_unique[,c(4,5,6,2,3)],
-                               edge.spells = animation_edges[,c(3,4,1,2,5)],
-                               create.TEAs=TRUE,edge.TEA.names='weight',
-                               vertex.TEA.names = c('pageRank', 'community'))
-get.change.times(g_netdyinamic)
-
-# See the density of most co-occurrentt citations from 2007 to 2021
-classNets <- get.networks(g_netdyinamic,start=2007,end=2021,time.increment=1,rule='latest')
-classDensity <- sapply(classNets, network.density)
-plot(classDensity,type='l',xlab='network slice #',ylab='density')
-weight_g3 = g3_net %e% "weight"
-
-# All edges find between 2007 and 2021
-plot( network.extract(g_netdyinamic, onset=2007, terminus=2021, rule="any"),
-      displaylabels=T,
-      #edge.lwd = (g3_net %e% "weight"),
-      edge.col = "lightgray") 
-
-coord_location[g3_net %v% "start" == 2007,]
-
-g3_net %e% "start"
-
-
-g_netdyinamic %v% "vertex.names" = animation_vertices_unique$name
-
-
-
-plot(g_netdyinamic)
-plot( network.extract(g_netdyinamic, at=2007), displaylabels=T)
-plot( network.extract(g_netdyinamic, at=2008), displaylabels=T)
-
-render.d3movie(g_netdyinamic, displaylabels=T) 
-
-# Aixo provenent d'un netowrk
-
-animation_edges_net = animation_edges
+animation_edges_net = animation_edges[,-5]
 colnames(animation_edges_net)[c(1,2,5)] = c("from", "to", "weight")
-animation_vertices_unique
-
 
 g3_net = network(animation_edges_net, matrix.type = "edgelist",
              vertex.attr = animation_vertices_unique,
@@ -227,50 +214,48 @@ g3_net = network(animation_edges_net, matrix.type = "edgelist",
              ignore.eval = F, directed = F )
 g3_net
 
-
 g3_net %v% "pageRank"
 g3_net %v% "community"
 g3_net %e% "weight"
 g3_net %v% "col" = rainbow(length(unique(g3_net %v% "community")))[as.factor(g3_net %v% "community")]
 
-plot(g3_net, vertex.cex=(g3_net %v% "pageRank")/10, vertex.col="col", edge.lwd = (g3_net %e% "weight")/10, edge.col = "lightgray")
 coord_location = plot(g3_net, vertex.cex=(g3_net %v% "pageRank")/30, vertex.col="col")
 
-plot(g3_net, vertex.cex=(g3_net %v% "pageRank")/30, vertex.col="col", coord = coord_location)
-plot(g3_net, vertex.cex=(g3_net %v% "pageRank")/30, vertex.col="col", coord = coord_location)
 
 es = data.frame(onset = as.numeric(animation_edges_net$start), terminus = animation_edges_net$end,
                 head=as.matrix(g3_net, matrix.type="edgelist")[,1],
                 tail=as.matrix(g3_net, matrix.type="edgelist")[,2])
-class(es$terminus)
 
 g3_dinamic_net =  networkDynamic(base.net=g3_net,
                                  edge.spells=es,
-                                 vertex.spells=animation_vertices_unique[,c(4,5,6)])
+                                 vertex.spells=animation_vertices_unique[,c(4,5,7)])
 
-plot(g3_dinamic_net, vertex.cex=(g3_net %v% "pageRank")/10, vertex.col="col",
-     edge.lwd = (g3_net %e% "weight")/10, edge.col = "lightgray", coord = coord_location,
-     edge.label=g3_net %e% "weight")
-
-g3_dinamic_net %v% "vertex.names" = animation_vertices_unique$name
+g3_dinamic_net %v% "vertex.names" = animation_vertices_unique$subname
+g3_dinamic_net %v% "pageRank" = animation_vertices_unique$pageRank
+g3_dinamic_net %v% "community" = animation_vertices_unique$community
+g3_dinamic_net %v% "real_name" = animation_vertices_unique$name
+g3_dinamic_net %v% "node_type" = animation_vertices_unique$node_type
+g3_dinamic_net %v% "node_shape" = c(50,4)[as.factor(g3_dinamic_net %v% "node_type")]
 
 
 comp_ani=compute.animation(g3_dinamic_net,
                            default.dist=3,
                            animation.mode = "Graphviz",
-                           slice.par=list(start=2007, end=2021, interval=1, 
+                           slice.par=list(start=min(g3_dinamic_net %e% "start"), end=2021, interval=1, 
                                  aggregate.dur=1, rule='any'),
                            verbose=FALSE, coord = coord_location)
-comp_mdsj = compute.animation(g3_dinamic_net, animation.mode = "MDSJ")
-render.d3movie(comp_mdsj, edge.label=(g3_net %e% "weight")/10)
-
-render.d3movie(comp_ani, layout.par=list(gv.engine='dot'), edge.label = (g3_net %e% "weight"), displaylabels = T)
 
 render.d3movie(comp_ani, displaylabels = T,
                vertex.cex=(g3_net %v% "pageRank")/10,
                vertex.col="col",
-               edge.lwd = (g3_net %e% "weight")/20, edge.col = "lightgray",
-               edge.label = g3_net %e% "weight",
-               launchBrowser=T,
-               render.par=list(tween.frames = 30, show.time = F),
-               edge.len= 1000)
+               vertex.sides = g3_dinamic_net %v% "node_shape",
+               edge.lwd = (g3_net %e% "weight")/75,
+               edge.col = "lightgray",
+               vertex.tooltip = paste("<b>Name:</b>", (g3_dinamic_net %v% "real_name") , "<br>",
+                                      "<b>Node type:</b>", (g3_dinamic_net %v% "node_type") , "<br>",
+                                      "<b>pageRank:</b>", (g3_dinamic_net %v% "pageRank") , "<br>",
+                                      "<b>Community:</b>", (g3_dinamic_net %v% "community")),
+               edge.tooltip = paste("<b>Cumulative sum:</b>", (g3_net %e% "weight") ),
+               launchBrowser=T,filename = "Animation.html"
+               )
+ 
