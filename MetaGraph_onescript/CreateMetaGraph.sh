d2173a81b6d9fa5cd84dfd361d8c8c56ce6e825f
Sergi <saguilocastillo@gmail.com>
2021-11-22 10:48:25 +0100

    Increase memory size

--- MetaGraph_onescript/CreateMetaGraph.sh
+++ MetaGraph_onescript/CreateMetaGraph.sh
@@ -24,7 +24,7 @@ echo "Extracting csv file from database"
 
 echo "Creating Neo4j database in docker with name: $Neo4jdatabasename"
 
-docker run --name $Neo4jdatabasename -p7474:7474 -p7687:7687 -d -v $PWD/$outputfolder:/var/lib/neo4j/import --env NEO4J_AUTH=none -e NEO4J_apoc_export_file_enabled=true -e NEO4J_apoc_import_file_enabled=true -e NEO4J_apoc_import_file_use__neo4j__config=true -e NEO4JLABS_PLUGINS='["apoc", "graph-data-science"]' neo4j:4.2.3 
+docker run --name $Neo4jdatabasename -p7474:7474 -p7687:7687 -d -v $PWD/$outputfolder:/var/lib/neo4j/import --env NEO4J_AUTH=none -e NEO4J_apoc_export_file_enabled=true -e NEO4J_apoc_import_file_enabled=true -e NEO4J_apoc_import_file_use__neo4j__config=true -e NEO4JLABS_PLUGINS='["apoc", "graph-data-science"]'  -e NEO4J_dbms_memory_heap_max__size=16G neo4j:4.2.3 
 
 sleep 30;
 
