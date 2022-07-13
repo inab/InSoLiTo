0373d4b68eddb0a0361fb5804fe8b6b5a128cb08
SergiAguilo <saguilocastillo@gmail.com>
2022-07-13 11:05:35 +0200

    Remove gathering of pmcid and add null when DOI not exist

--- DB/SQLiteScripts/RetrievePublications.py
+++ DB/SQLiteScripts/RetrievePublications.py
@@ -12,10 +12,10 @@ def retrieve_publications(mypath,c):
     # For each file
     for files in filenames:
         filepath = f"{mypath}/{files}"
-        print(f"Parsing {filepath}")
         # Not compressed files are not parsed (Ex: Index files)
         if not filepath.endswith("xml.gz"):
             continue
+        print("Parsing", filepath)
         # Open the gzip and do the calculations
         with gzip.open(filepath,"r") as f:
             # Read XML file
@@ -23,13 +23,13 @@ def retrieve_publications(mypath,c):
             # For all the publications in the file
             for article in root.iter("PubmedArticle"):
                 # Initialize variables where the ids will be stored
-                pubmed_id = ""
-                doi_id = ""
-                pmcid_id = ""
+                pubmed_id = None
+                doi_id = None
                 # Search and store the title
                 for title_atr in article.iter("ArticleTitle"):
                     title = ''.join(title_atr.itertext())   # Take all text and the tags inside them
                     title = title.replace('"', "'")         # Standarize the quotation marks
+                
                 # If no title, do not store article
                 if not title:
                     continue
@@ -49,16 +49,17 @@ def retrieve_publications(mypath,c):
                             pubmed_id = Id_atr.text
                         if "doi" in Id_atr.attrib["IdType"]:
                             doi_id = Id_atr.text
-                        if "pmcid" in Id_atr.attrib["IdType"]:
-                            pmcid_id = Id_atr.text
                     break
                 # If article has any ID
-                if pmcid_id or pubmed_id or doi_id:
+                if not pubmed_id:
+                    continue
+                if doi_id:
                     #Insert the variables into the database, in the table Publications
                     c.execute(f'''INSERT OR REPLACE INTO Publications
-                                VALUES ("{title}",{year},"{pmcid_id}", "{pubmed_id}", "{doi_id}")''')
+                                VALUES ("{title}",{year}, {pubmed_id}, "{doi_id}")''')
+                else:
+                    #Insert the variables into the database, in the table Publications
+                    c.execute(f'''INSERT OR REPLACE INTO Publications
+                                VALUES ("{title}",{year}, {pubmed_id}, null)''')                    
                     counter+=1
-                    print(counter)
     print(f"There are {counter} publications in the database")
-
-# 6h 15 min
