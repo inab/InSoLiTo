60ed094dd125dbf0ff8ffa8d0936c92720c60978
OMGM051 <marcos.mar.fer051@gmail.com>
2025-03-10 17:11:47 +0100

    Added responsiveness and a little animation to make less harsh the transition

--- FRONTEND/src/modules.js/navBar.js
+++ FRONTEND/src/modules.js/navBar.js
@@ -68,6 +68,10 @@ const actionSidebar = () => {
         $("#visualization").removeClass("visualization-without-sidebar");
       }
       $("#visualization").addClass("visualization-with-sidebar");
+      if ($("#liveAlertPlaceholder").hasClass("live-alert-placeholder-without-sidebar")) {
+        $("#liveAlertPlaceholder").removeClass("live-alert-placeholder-without-sidebar");
+      }
+      $("#liveAlertPlaceholder").addClass("live-alert-placeholder-with-sidebar");
     } else {
       // Close the sidebar
       if ($("#mySidebar").hasClass("sidebar-open")) {
@@ -83,6 +87,10 @@ const actionSidebar = () => {
         $("#visualization").removeClass("visualization-with-sidebar");
       }
       $("#visualization").addClass("visualization-without-sidebar");
+      if ($("#liveAlertPlaceholder").hasClass("live-alert-placeholder-with-sidebar")) {
+        $("#liveAlertPlaceholder").removeClass("live-alert-placeholder-with-sidebar");
+      }
+      $("#liveAlertPlaceholder").addClass("live-alert-placeholder-without-sidebar");
     }
     // Add the new image to the button
     button.append(buttonImage);
