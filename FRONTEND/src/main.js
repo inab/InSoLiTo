c697610450752220c49ba9aea8843ac16aeee495
Juan Crisantos Pérez Carrasco <juancrisantos.perez@gmail.com>
2025-03-12 13:16:49 +0100

    Add new functionality to reset button. When click, the app now redirect to home page

--- FRONTEND/src/main.js
+++ FRONTEND/src/main.js
@@ -343,6 +343,7 @@ try {
   $("#reset").on("click", () => {
     removeAllTopicsMenu();
     reset();
+    $("#inital-screen").removeClass("hidden");
   });
 
   // Attach a click event handler to the element with the ID "stabilize".
